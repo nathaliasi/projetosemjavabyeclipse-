34741ff327dc3721cc0a639292868ea3de255c6f
Nathália Sardou <nathaliatrab@outlook.com>
2021-12-02 12:45:50 -0300

    PrimeiroPrograma java
    olá mundo

---
+++ pacote download/primeiro-projeto/src/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+
+public class PrimeiroPrograma {
+	
+	//ponto de entrada para nossa aplicação
+	public static void main(String[] args){
+		System.out.println("olá mundo ");
+	//variaáveis do tipo inteiro
+		int idade;
+		idade=25;
+		System.out.println("A idade é" +idade);
+	}
+
+}
